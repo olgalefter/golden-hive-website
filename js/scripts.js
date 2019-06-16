fe4fc4bcfbf03c83bd5ddc16c818b8c4a5c23d15
Olga <olga.daschevici@gmail.com>
2019-06-16 16:51:33 -0500

    Add a responsive nav bar.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,8 @@
-console.log("Welcome to Golden Hive Apiary!");
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
