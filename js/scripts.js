b90365df64d16e2b179fee88f6ff191cc9f10b47
Olga <olga.daschevici@gmail.com>
2019-06-16 21:54:29 -0500

    Add responsive navbar.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,34 @@
-console.log("Welcome to Golden Hive Apiary!");
+$(document).ready(function() {
+
+  // Create the show/hide function
+  $("#navToggle a").click(function(e) {
+    e.preventDefault();
+
+    $("header > nav").slideToggle();
+    $("#logo").toggleClass("menuUp menuDown");
+  });
+
+  // Create Drop Down Menu for Mobile Devices
+  $("header > nav > ul > li > a").click(function(e) {
+    if ($(window).width() < "768") {
+      if ($(this).siblings().size() > 0) {
+        e.preventDefault();
+        $(this).siblings().slideToggle("fast")
+        $(this).children(".toggle").html($(this).children(".toggle").html() == 'close' ? 'expand' : 'close');
+      }
+    }
+  });
+
+  // Create a tidy up function for if the user decides to play with the screen size between mobile and desktop.
+  $(window).resize(function() {
+    if ($(window).width() > "768") {
+      $("header > nav").css("display", "block");
+
+      if ($("#logo").attr('class') == "menuDown") {
+        $("#logo").toggleClass("menuUp menuDown");
+      }
+    } else {
+      $("header > nav").css("display", "none");
+    }
+  });
+});
