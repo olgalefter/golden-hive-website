78f34fd91f11cd2502262723b3f00490e057272f
Olga <olga.daschevici@gmail.com>
2019-06-19 19:56:26 -0500

    Add responsive nav bar.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,28 @@
-function myFunction() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
+function toggleTopnav() {
+  var topnav = document.getElementById("topnav");
+  var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
+  if (topnav.classList.contains("responsive")) {
+    topnav.classList.remove("responsive");
+    dropdownContent.classList.remove("responsive");
   } else {
-    x.className = "topnav";
+    topnav.classList.add("responsive");
   }
 }
+
+function toggleDropdown() {
+  var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
+  if (dropdownContent.classList.contains("responsive")) {
+    dropdownContent.classList.remove("responsive");
+  } else {
+    dropdownContent.classList.add("responsive");
+  }
+}
+//
+// function toggleDropdown() {
+//   var dropdownContent = document.getElementsByClassName("dropdown-content")[0];
+//   if(dropdownContent.style.display == "none") {
+//     dropdownContent.style.display = "block";
+//   } else {
+//     dropdownContent.style.display = "none";
+//   }
+// }
